cef45c138c78a7512e80386cd4d6db35779012e5
Clem21 <clemence.pereira@hotmail.fr>
2020-02-27 14:40:08 +0100

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo-app';
+
+export class AppComponent implements OnInit {
+  public todo;
+  public inputData;
+  public show: boolean;
+
+  constructor(private http: HttpClient) {
+    this.todo = [];
+    this.show = false;
+  }
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.show = !this.show;
+  }
+
+  add() {
+    this.todo.push(this.inputData);
+  }
+
+  delete(index) {
+    this.todo.splice(index, 1);
+  }
 }
